8a997e463340db29ce47c2382fc6c209672d68a2
Chsfdjr <114878424+Chsfdjr@users.noreply.github.com>
2023-06-27 10:22:46 +0200

    add variables

--- server/routes/auto_refresh.js
+++ server/routes/auto_refresh.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json(); 
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
     const link = req.body.link;
+    const delay = req.body.delay;
 
     if (!link) {
         console.log("no link inside");
         return res.status(404).json('Empty link');
     } else {
-        
+
     }
 });
