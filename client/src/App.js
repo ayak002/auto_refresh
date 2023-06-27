710cd88bd2df7f68f0c1b96a790eb1b4c0467641
Chsfdjr <114878424+Chsfdjr@users.noreply.github.com>
2023-06-27 12:43:27 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState} from 'react';
+import { BrowserRouter as Router, Route, Switch as RSwitch } from 'react-router-dom';
 
-function App() {
+const Autorefresh = (props) => {
+  const [link, setLink] = useState('');
+  const [delay, setDelay] = useState('');
+
+  const handleSubmit = async (check_data) => {
+    check_data.preventDefault();
+    const request = {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify ({
+        link,
+        delay,
+      })
+    };
+    console.log(link, delay);
+    try {
+      const response = await fetch('/', request);
+      const data = await response.json();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label><p></p>
+        <input onChange={(check_data) => setLink(check_data.target.value)} type="text" placeholder="Link" /><p></p>
+        <label htmlFor="password">Password</label><p></p>
+        <input onChange={(check_data) => setDelay(check_data.target.value)} type="text" placeholder="Delay" /><p></p>
+        <button type="submit">Submit</button>
+      </form>
     </div>
+  );
+}
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+      <RSwitch>
+        <Route exact path="/" component={Autorefresh} />
+      </RSwitch>
+      </div>
+    </Router>
   );
 }
 
--- server/routes/auto_refresh.js
+++ server/routes/auto_refresh.js
@@ -2,15 +2,93 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json(); 
+const db = require('/../config/db');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Link = "";
+const Delay = "";
+const CurrentCode = "";
+const PreviousCode = "";
 
-router.post("/", async (req, res) => {
-    const link = req.body.link; // récupération du lien et le stocker dans la variable link
-    const delay = req.body.delay; // //stocker le nb de secondes dans un variable delay
-
-    if (!link) {
+const handle_link = (link, next) => {
+    if (link[0].length == 0) {
         console.log("no link inside");
         return res.status(404).json('Empty link');
     } else {
+        next();
+    }
+}
 
+const handle_delay = (delay, next) => {
+    if (delay[0].length == 0) {
+        console.log("no delay inside");
+        return res.status(404).json('Delay link');
+    } else {
+        next();
+    }
+}
+
+router.post("/", jsonParser, async (req, res) => {
+    const link = req.body.link;
+    const delay = req.body.delay;
+    console.log(`console dans server : ${link, delay}`);
+    try {
+        handle_link(link);
+        handle_delay(delay);
+        const [data] = await db.promise().query('INSERT INTO a_refresh (link, delay) VALUES (?, ?)', [
+            link,
+            delay
+        ]);
+        Link = link;
+        Delay = delay;
+        res.status(200).json(`${link} catched succesfully and refresh in ${delay}`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Internal server error');     
+    }
+});
+
+router.get('/', jsonParser, async (req, res) => {
+    try {
+        const link = await db.promise().query('SELECT * FROM a_refresh WHERE link = ?', [Link]);
+        const delay = await db.promise().query('SELECT * FROM a_refresh WHERE delay = ?', [Delay]);
+
+        if (link[0].length == 0 || delay[0].length == 0) {
+            return res.status(404).json('Not found');
+        }
+        const reponse = await axios.get(link);
+        if (CurrentCode) {
+            PreviousCode = CurrentCode;
+            CurrentCode = response.data;
+        } else {
+            CurrentCode = response.data;
+        }
+        if (PreviousCode && (CurrentCode === PreviousCode)) {
+            return res.status(852).json("MODIFICATION");
+        } else {
+            return res.status(853).json("REFRESH");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Internal server error');
+    }
+});
+
+router.delete('/', auth, jsonParser, async (req, res) => {
+    try {
+        const link = await db.promise().query('SELECT * FROM a_refresh WHERE link = ?', [Link]);
+        const delay = await db.promise().query('SELECT * FROM a_refresh WHERE delay = ?', [Delay]);
+        
+        if (link[0].length == 0 || delay[0].length == 0) {
+            return res.status(404).json('Not found');
+        }
+        const delete_link = await db.promise().query('DELETE from user where id = ?', [link]);
+        const delete_delay = await db.promise().query('DELETE from user where id = ?', [delay]);
+        if (delete_link && delete_delay) {
+            return res.status(200).json("Successfully reset");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Internal server error');
     }
 });
